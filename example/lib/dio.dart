dede4dfee75c445f8e5aa17ab3933e3027469c64
Marcelo Milhomem <marcelo@loadsmile.com>
2021-06-20 17:40:18 +1000

    Adds simple example for checking httpstatus with DIO
    As requested by the community I'm adding a simple example of making a
    call with DIO and getting the HTTP Response code to validate if we have
    to upgrade or not.
    
    DIO has a particular way for handling exceptions [1] that needs to by
    try catch to check status codes that are considered errors.
    
    Closes #18
    
    [1]: https://pub.dev/packages/dio#handling-errors

---
+++ example/lib/dio.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:native_updater/native_updater.dart';
+import 'package:dio/dio.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'native_updater DIO example',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<void> checkVersion(int statusCode) async {
+    /// For example: You got status code of 412 from the
+    /// response of HTTP request.
+    /// Let's say the statusCode 412 requires you to force update
+    Future.delayed(Duration.zero, () {
+      if (statusCode == HttpStatus.unauthorized;) {
+        NativeUpdater.displayUpdateAlert(
+          context,
+          forceUpdate: true,
+          appStoreUrl: '<Your App Store URL>',
+          playStoreUrl: '<Your Play Store URL>',
+          iOSDescription: '<Your iOS description>',
+          iOSUpdateButtonLabel: 'Upgrade',
+          iOSCloseButtonLabel: 'Exit',
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your App'),
+      ),
+      body: Center(
+        child: FlatButton(
+            onPressed: requestAPI,
+            child: Text('Request API')
+        ),
+      ),
+    );
+  }
+
+  requestAPI() {
+    var dio = Dio(BaseOptions(
+      baseUrl: 'http://httpbin.org/',
+    ));
+
+    try {
+      Response response = await dio.get('/get');
+    } on DioError catch (e) {
+      checkVersion(e.response.statusCode);
+    }
+  }
+}
